c9d9c2a0434917e0628d24a3084bb7d32dc7f182
Denis <denis@infrabh.com.br>
2023-12-23 12:09:46 -0300

    corrigindo identacao

--- desafio_02.js
+++ desafio_02.js
@@ -1,28 +1,25 @@
-let saldoVitorias = rank(100,22)
+let saldoVitorias = rank(100, 22)
 let nivel = ""
 
-if(saldoVitorias < 10){
+if (saldoVitorias < 10) {
     nivel = "FERRO"
-}else if((saldoVitorias >= 11) && (saldoVitorias <=20)){
+} else if ((saldoVitorias >= 11) && (saldoVitorias <= 20)) {
     nivel = "BRONZE"
-}if((saldoVitorias >=21) && (saldoVitorias <=50)){
+} if ((saldoVitorias >= 21) && (saldoVitorias <= 50)) {
     nivel = "PRATA"
-}else if((saldoVitorias >=51) && (saldoVitorias <=80)){
+} else if ((saldoVitorias >= 51) && (saldoVitorias <= 80)) {
     nivel = "OURO"
-}if(saldoVitorias >=81 && saldoVitorias <=90){
+} if (saldoVitorias >= 81 && saldoVitorias <= 90) {
     nivel = "DIAMANTE"
-}else if(saldoVitorias >=91 && saldoVitorias <=100){
+} else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
     nivel = "LENDARIO"
-}if(saldoVitorias >= 101){
+} if (saldoVitorias >= 101) {
     nivel = " IMORTAL"
 }
 
 console.log("O Heroi tem saldo de " + saldoVitorias + " esta no nivel " + nivel)
 
-
-
-
-function rank(numVitoria, numDerrota){
+function rank(numVitoria, numDerrota) {
     let rankVitoria = numVitoria - numDerrota
     return rankVitoria
 }
