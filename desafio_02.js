27f87e28981c7aa9b119dd6f63dac5a5eeedfabf
Denis <denis@infrabh.com.br>
2023-12-23 10:57:28 -0300

    alinhamento nome arquivos entrega projetos dio

---
+++ desafio_02.js
@@ -0,0 +1,23 @@
+let nomeDoHeroi = "Jar Jar Binks"
+let nivel = 6425
+let experiencia = ""
+
+if (nivel <= 1000) {
+    experiencia = "FERRO"
+} else if ((nivel >= 1001) && (nivel <= 2000)) {
+    experiencia = "BRONZE"
+} if ((nivel >= 2001) && (nivel <= 5000)) {
+    experiencia = "PRATA"
+} else if ((nivel >= 6001) && (nivel <= 7000)) {
+    experiencia = "OURO"
+} if ((nivel >= 7001) && (nivel <= 8000)) {
+    experiencia = "PLATINA"
+} else if ((nivel >= 8001) && (nivel <= 9000)) {
+    experiencia = "ASCENDENTE"
+} if ((nivel >= 9001) && (nivel <= 10000)) {
+    experiencia = "IMORTAL"
+} else if (nivel >= 10001) {
+    experiencia = "RADIANTE"
+}
+
+console.log("O Herói: " + nomeDoHeroi + " esta no nivel: " + experiencia)
